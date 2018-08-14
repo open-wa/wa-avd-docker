090cb54a2370cfbab26a2ae0dd306cf6b69a537a
BenSchZA <bscholtz.bds@gmail.com>
2018-08-14 18:15:07 +0200

    Create AVD during build

--- post-build.sh
+++ post-build.sh
@@ -9,5 +9,4 @@ wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -P /ap
 	&& yes 'A' | unzip /app/sdk-tools-linux-4333796.zip -d ${ANDROID_HOME} \
 	&& yes | ${ANDROID_HOME}/tools/bin/sdkmanager \
 	"build-tools;28.0.2" "sources;android-26" "platform-tools" "platforms;android-26" "system-images;android-26;google_apis;x86" \
-	#&& echo no | ${ANDROID_HOME}/tools/bin/avdmanager create avd -n Pixel -k "system-images;android-26;google_apis;x86" \
-	#-p ${ANDROID_AVD_HOME} -c 2000M
+	&& echo no | ${ANDROID_HOME}/tools/bin/avdmanager create avd -n Pixel -k "system-images;android-26;google_apis;x86" -c 2000M
